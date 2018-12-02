68bdf8366fa391342b2819f121795fd0c6d53398
Michael Ho <michael@gem.co>
2018-12-01 23:39:49 -0800

    Add impl for PoS

--- pos/pos.py
+++ pos/pos.py
@@ -1,23 +1,38 @@
 from pprint import pprint
 import spacy
 
-nlp = spacy.load("en")
+TEXTS = [
+    'S. flavida decreased G0/G1 arrest and facilitated the G2/M transition of NIH3T3s, also downregulated the expression of p38, p53, p16, and the related inflammatory mediators',
+    'S. flavida potentially modulated senescence-associated hallmarks in fibroblasts exposed to H2 O2 , thus it may inhibit the aging process via controlling the OS',
+]
 
-filename = "raw"
 
-document = open(filename).read()
-document = nlp(document)
+def main(model='en_core_web_sm'):
+    print("Start app")
+    nlp = spacy.load(model)
+    print("Loaded model '%s'" % model)
+    print("Processing %d texts" % len(TEXTS))
 
-# pprint(document)
-# pprint(list(document.sents))
+    for text in TEXTS:
+        doc = nlp(text)
+        relations = extract_relations(doc)
+        for r1, r2, r3 in relations:
+            print('{:<10}\t{}\t{}'.format(r1.text, r3, r2.lemma_))
+            # print('{:<10}\t{}\t{}'.format(r1.text, r3, r2.text))
 
-# pprint(list(document.ents))
+def extract_relations(doc):
+    # merge entities and noun chunks into one token
+    # TODO: Why removing this stops the method from functioning
+    spans = list(doc.ents) + list(doc.noun_chunks)
+    for span in spans:
+        span.merge()
 
-sentence = document[3]
+    relations = []
+    # for token in filter(lambda w: str(w) == "S. flavida", doc):
+    for token in doc:
+        if token.dep_ == 'nsubj':
+            relations.append((token.head.right_edge, token.head, token))
+    return relations
 
-print(document.sents)
-
-# for sent in document.sents:
-#     for word in sent.word:
-#         print(list(word.children))
-#     # print(ent , " " , ent.label_)
+if __name__ == "__main__":
+    main()
