547745be40fe7669149dee7c0d62455aff29636e
Michael Ho <michael@gem.co>
2018-12-02 00:21:38 -0800

    Update pos to read from file

--- pos/pos.py
+++ pos/pos.py
@@ -1,24 +1,18 @@
 from pprint import pprint
 import spacy
 
-TEXTS = [
-    'S. flavida decreased G0/G1 arrest and facilitated the G2/M transition of NIH3T3s, also downregulated the expression of p38, p53, p16, and the related inflammatory mediators',
-    'S. flavida potentially modulated senescence-associated hallmarks in fibroblasts exposed to H2 O2 , thus it may inhibit the aging process via controlling the OS',
-]
-
-
 def main(model='en_core_web_sm'):
     print("Start app")
     nlp = spacy.load(model)
     print("Loaded model '%s'" % model)
-    print("Processing %d texts" % len(TEXTS))
 
-    for text in TEXTS:
-        doc = nlp(text)
-        relations = extract_relations(doc)
+    document = open("raw").read()
+    document = nlp(document)
+
+    for text in document.sents:
+        relations = extract_relations(text)
         for r1, r2, r3 in relations:
             print('{:<10}\t{}\t{}'.format(r1.text, r3, r2.lemma_))
-            # print('{:<10}\t{}\t{}'.format(r1.text, r3, r2.text))
 
 def extract_relations(doc):
     # merge entities and noun chunks into one token
@@ -32,6 +26,9 @@ def extract_relations(doc):
     # Extract subject, verb, object
     for token in doc:
         if token.dep_ == 'nsubj':
+            # print("is nsubj", token)
+            # print(token.head)
+            # print(token.head.right_edge)
             relations.append((token.head.right_edge, token.head, token))
     return relations
 
